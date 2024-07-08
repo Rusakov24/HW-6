f2ef90552fba4ecac2b02f7a4d636a7ad20ad3f3
Rusakov24 <lovewillkillme@vk.com>
2024-07-08 13:35:33 +0300

    Начало работы

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Интерация цикла " + i);
+        }
+        System.out.println("Задание 2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("Интерация цикла " + i);
+        }
+        System.out.println("Задание 3");
+    }
+}
